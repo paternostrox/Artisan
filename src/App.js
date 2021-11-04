3001e1d8427c4d94cbfdd4154f80ea18f69ae74b
pedro <pedro.paternostro@gmail.com>
2021-11-04 10:55:42 -0300

    Project added, currenly a sidebar w/ views

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './pages/Home'
+import Products from './pages/Products'
+import Reports from './pages/Reports'
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route path='/' exact component={Home}/>
+          <Route path='/products' component={Products}/>
+          <Route path='/reports' component={Reports}/>
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
